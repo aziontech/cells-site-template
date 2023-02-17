d3b6228b828f6bac73f6dae88e2ef3a97eeaa6a0
enicio <jessandro42@gmail.com>
2023-02-17 15:30:03 -0300

    Change index.js to use cells fetch api

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
-import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
-
-/**
- * The DEBUG flag may be helpful during development:
- * - we will return an error message on exception in your Response rather than
- *   the default 404.html page.
- */
-const DEBUG = false
-
 addEventListener('fetch', event => {
-  event.respondWith(handleEvent(event))
+  event.respondWith(handleEvent(event.request))
 })
 
-async function handleEvent(event) {
+async function handleEvent(request) {
   try {
-    let options = {
-      ASSET_NAMESPACE: __STATIC_CONTENT,
-      ASSET_MANIFEST: __STATIC_CONTENT_MANIFEST
-    }
-    return await getAssetFromKV(event, options)
+    const request_path = new URL(request.url).pathname;
+    const asset_url = new URL(request_path === "/" ? "index.html" : request_path, "file://");
+    const response = await fetch(asset_url);
+    return new Response(await response.blob());
   } catch (e) {
-    if (!DEBUG) {
-      try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
-        })
-
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
-      } catch (e) {}
-    }
-
     return new Response(e.message || e.toString(), { status: 500 })
   }
 }
