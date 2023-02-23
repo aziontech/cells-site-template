8bfce27729d03acc5349a39072b5515090d03349
enicio <jessandro42@gmail.com>
2023-02-22 22:49:01 -0300

    refactor function handleEvent

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleEvent(event.request))
+  event.respondWith(handleEvent(event))
 })
 
-async function handleEvent(request) {
+async function handleEvent(event) {
   try {
-    const request_path = new URL(request.url).pathname;
-    const asset_url = new URL(request_path === "/" ? "index.html" : request_path, "file://");
-    const response = await fetch(asset_url);
-    return new Response(await response.blob());
+    const request_path = new URL(event.request.url).pathname;
+    const asset_url = new URL(request_path === "/" ? `${VERSION_ID}/index.html` : `${VERSION_ID}${request_path}`, `file://`);
+    return fetch(asset_url);
+
   } catch (e) {
     return new Response(e.message || e.toString(), { status: 500 })
   }
